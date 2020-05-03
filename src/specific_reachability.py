280bccebcb21e02a02d5e7cda5c4d9787b654c90
Felix Ocker <felix.ocker@googlemail.com>
2020-05-03 23:22:43 +0200

    provide feasibility feedback w explanation via stdout

--- src/specific_reachability.py
+++ src/specific_reachability.py
@@ -11,6 +11,7 @@ ONTOFILE = "logistics-onto.owl"
 IRI = "http://example.org/logistics-onto.owl"
 PREFIXES = "../queries/prefixes.sparql"
 QUERY = "../queries/find_viable_transport_resources.sparql"
+QUERY_FEATS = "../queries/feature_feasibility.sparql"
 
 def run_query(ontofile, query_body):
     """build query and run on ontofile"""
@@ -30,5 +31,14 @@ def insert_relations(specific_relations):
             onto[str(i[0]).split('.')[-1]].specifically_connected_to.append(onto[str(i[1]).split('.')[-1]])
     onto.save(file=ONTOFILE)
 
+def specific_feedback(infeasible_features):
+    """return feedback which features cannot be realized"""
+    for i in infeasible_features:
+        if not i[1]:
+            print(i[0], " - infeasible - no available resource can realize this feature")
+        else:
+            print(i[0], " - infeasible - no transport connection to resource that can realize ", i[1])
+
 if __name__ == "__main__":
     insert_relations(run_query(ONTOFILE, QUERY))
+    specific_feedback(run_query(ONTOFILE, QUERY_FEATS))
